1ed9f0bfbb962b0ed17e4e300fab80d94935be43
Anna Langner <annalangner06@gmail.com>
2019-08-07 13:15:04 +0200

    Separating routes to a separate file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import {Router, Route, hashHistory } from "react-router";
+import {Router, hashHistory } from "react-router";
 import { Provider } from 'react-redux';
 import store from './store';
-import Navigation from './presentional/navigation.component';
+import routes from './routes';
 
 render(
   <Provider store={store}>
-      <Router history={hashHistory}>
-          <Route path={'/'} component={Navigation}/>
-      </Router>
+      <Router history={hashHistory} routes={routes}/>
   </Provider>,
   document.getElementById('root')
 );
