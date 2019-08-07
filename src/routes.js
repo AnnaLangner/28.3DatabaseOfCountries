a929fc1b2405d6c273faa0339e7cd2f8f69a4982
Anna Langner <annalangner06@gmail.com>
2019-08-07 14:00:57 +0200

    Adding new routes

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,14 @@
 import React from  'react';
-import { Route } from "react-router";
+import { Route, IndexRoute } from "react-router";
 import Navigation from './presentional/navigation.component';
+import Home from './presentional/home.component';
+import Contact from './presentional/contact.component';
+import NotFound from  './presentional/not-found.component';
 
 export default (
-    <Route path={'/'} component={Navigation}>
-
+    <Route path='/' component={Navigation}>
+        <IndexRoute component={Home}/>
+        <Route path='contact' component={Contact}/>
+        <Route path='*' component={NotFound}/>
     </Route>
 );
